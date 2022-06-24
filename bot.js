29a3273f3708bd400c4bac2f8781539eca6acc97
golafoned <golafoned@gmail.com>
2022-06-24 14:18:24 +0300

    intall try

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5306549693:AAGwZ-6F-589xONPdx5t3v4OJBCVj_CWK9g';
 const bot = new TelegramBot(token, {polling: true});
+console.log('bot started')
 function solut(msg) {
     try {
     	if (typeof(eval(msg)) != 'number') return ('Somth went wrong. Try again')
